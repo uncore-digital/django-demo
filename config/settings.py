fbfdf94b1f6d43617d8026a7e0435d90da4b85cd
Eli North <admin@uncore.com>
2023-04-29 07:36:14 -0500

    remove secret_key env value

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = get_random_secret_key()
+# SECRET_KEY = env('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
