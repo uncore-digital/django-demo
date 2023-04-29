3aafe98f1be373b2f840eeeadbe79751753c139a
Eli North <admin@uncore.com>
2023-04-29 06:56:18 -0500

    Migrate project from 'django-rest-docker'

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,11 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+## This will return the proper URL pattern for serving static files to your already defined pattern list. Use it like this:
+## https://docs.djangoproject.com/en/4.2/ref/contrib/staticfiles/
+urlpatterns += staticfiles_urlpatterns()
